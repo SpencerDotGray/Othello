eddcfc88930fc58dda874db9c33a93af4321631e
Spencer Gray <grayspa17@gmail.com>
2021-04-08 14:14:44 -0400

    Added AI random move

--- backend/controller.py
+++ backend/controller.py
@@ -1,5 +1,6 @@
 
 from backend.slothandler import Slothandler
+import random
 
 class Controller:
 
@@ -7,18 +8,26 @@ class Controller:
         self.app = app
         self.slothandler = slothandler
     
-    def place(row, col):
+    def place(self, row, col):
         self.slothandler.place(row, col)
     
-    def can_place(row, col) -> bool:
+    def can_place(self, row, col) -> bool:
         return self.slothandler.can_place(row, col)
     
-    def is_game_over() -> bool:
+    def is_game_over(self) -> bool:
         return self.slothandler.go
     
-    def new_game():
+    def new_game(self):
         self.slothandler.new_game()
     
-    def get_available_moves():
+    def get_available_moves(self):
         self.slothandler.availableMovesSignal.emit()
         return self.slothandler.moves
+    
+    def _ai_random_move(self):
+        moves = self.get_available_moves()
+        move = moves[random.randint(0, len(moves)-1)]
+
+    
+    def get_available_moves_with_place(row, col):
+        self.
