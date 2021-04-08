daac0c5fd1f86a980be5e4f53e197f2bb0b1b280
Spencer Gray <grayspa17@gmail.com>
2021-04-08 09:20:35 -0400

    Updated gitignore

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 
 python --version
-python -m venv amc_env
-call .\amc_env\Scripts\activate venv & pip install -r requirements.txt & cls & python main.py
+python -m venv othello_env
+call .\othello_env\Scripts\activate venv & pip install -r requirements.txt & cls & python main.py
--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ else
 fi
 
 python${versionSuffix} --version
-python${versionSuffix} -m venv amc_env
-source ./amc_env/bin/activate venv & pip${versionSuffix} install -r requirements.txt 
+python${versionSuffix} -m venv othello_env
+source ./othello_env/bin/activate venv & pip${versionSuffix} install -r requirements.txt 
 clear & python${versionSuffix} main.py
